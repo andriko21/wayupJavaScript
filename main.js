914ce1e204b5a8d5be9edb8531bb4d9c90dde3fb
andriko21 <75207634+andriko21@users.noreply.github.com>
2022-02-15 22:08:34 +0200

    first homework

---
+++ main.js
@@ -0,0 +1,62 @@
+const city = {
+  name: "Kremenets",
+  country: "Ukraine",
+  population: 20827,
+  isFootballStadium: true,
+};
+
+// console.log("task 1", city);
+
+function area() {
+  let width = 70,
+    height = 40;
+
+  const totalArea = width * height;
+  return totalArea;
+}
+
+// console.log("task 2", area());
+
+function cars() {
+  let time = 2,
+    speedOfFirst = 95,
+    speedOfSecond = 114;
+
+  const distance = speedOfFirst * time + speedOfSecond * time;
+  return distance;
+}
+
+// console.log("task 3", cars());
+
+function randomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100);
+
+  if (randomNumber < 20) {
+    console.log("randomNumber меньше 20");
+  } else if (randomNumber > 50) {
+    console.log("randomNumber больше 50");
+  } else {
+    console.log("randomNumber больше 20, и меньше 50");
+  }
+}
+
+// console.log("task 4", randomNumber());
+
+function updateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100);
+
+  switch (true) {
+    case (randomNumber < 20):
+      console.log("randomNumber меньше 20");
+      break;
+
+    case (randomNumber > 50):
+      console.log("randomNumber больше 50");
+      break;
+
+    default:
+      console.log("  randomNumber больше 20, и меньше 50");
+  }
+}
+
+// console.log("task 5", updateRandomNumber());
