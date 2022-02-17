9996bbdd5bbeeb093205346e7a812f5d769dfca5
andriko21 <75207634+andriko21@users.noreply.github.com>
2022-02-17 20:17:30 +0200

    homework2

--- homework 1/main.js
+++ homework 1/main.js
@@ -1,62 +1,62 @@
-// const city = {
-//   name: "Kremenets",
-//   country: "Ukraine",
-//   population: 20827,
-//   isFootballStadium: true,
-// };
+const city = {
+  name: "Kremenets",
+  country: "Ukraine",
+  population: 20827,
+  isFootballStadium: true,
+};
 
-// // console.log("task 1", city);
+// console.log("task 1", city);
 
-// function area() {
-//   let width = 70,
-//     height = 40;
+function area() {
+  let width = 70,
+    height = 40;
 
-//   const totalArea = width * height;
-//   return totalArea;
-// }
+  const totalArea = width * height;
+  return totalArea;
+}
 
-// // console.log("task 2", area());
+// console.log("task 2", area());
 
-// function cars() {
-//   let time = 2,
-//     speedOfFirst = 95,
-//     speedOfSecond = 114;
+function cars() {
+  let time = 2,
+    speedOfFirst = 95,
+    speedOfSecond = 114;
 
-//   const distance = speedOfFirst * time + speedOfSecond * time;
-//   return distance;
-// }
+  const distance = speedOfFirst * time + speedOfSecond * time;
+  return distance;
+}
 
-// // console.log("task 3", cars());
+// console.log("task 3", cars());
 
-// function randomNumber() {
-//   const randomNumber = Math.floor(Math.random() * 100);
+function randomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100);
 
-//   if (randomNumber < 20) {
-//     console.log("randomNumber меньше 20");
-//   } else if (randomNumber > 50) {
-//     console.log("randomNumber больше 50");
-//   } else {
-//     console.log("randomNumber больше 20, и меньше 50");
-//   }
-// }
+  if (randomNumber < 20) {
+    console.log("randomNumber меньше 20");
+  } else if (randomNumber > 50) {
+    console.log("randomNumber больше 50");
+  } else {
+    console.log("randomNumber больше 20, и меньше 50");
+  }
+}
 
-// // console.log("task 4", randomNumber());
+// console.log("task 4", randomNumber());
 
-// function updateRandomNumber() {
-//   const randomNumber = Math.floor(Math.random() * 100);
+function updateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100);
 
-//   switch (true) {
-//     case (randomNumber < 20):
-//       console.log("randomNumber меньше 20");
-//       break;
+  switch (true) {
+    case (randomNumber < 20):
+      console.log("randomNumber меньше 20");
+      break;
 
-//     case (randomNumber > 50):
-//       console.log("randomNumber больше 50");
-//       break;
+    case (randomNumber > 50):
+      console.log("randomNumber больше 50");
+      break;
 
-//     default:
-//       console.log("  randomNumber больше 20, и меньше 50");
-//   }
-// }
+    default:
+      console.log("  randomNumber больше 20, и меньше 50");
+  }
+}
 
-// // console.log("task 5", updateRandomNumber());
+// console.log("task 5", updateRandomNumber());
--- homework 2/main.js
+++ homework 2/main.js
@@ -1,11 +1,60 @@
 function task1() {
-  for (let i = 10; i < 50; i++) {
-    if (i / 2 !== 0) {
-        return;
-      }
+  for (let i = 10; i <= 50; i++) {
+    if (i % 2 === 0) {
       console.log(i);
     }
-    
+  }
+}
+task1();
+
+const aboutMe = {
+  name: "Andrii",
+  surname: "Belinskii",
+  age: 14,
+  isPet: true,
+};
+
+console.log(aboutMe);
+
+function task3() {
+  const array = [
+    "я в Симбирск,",
+    "в трактире.",
+    "с утра",
+    "В ту же ночь",
+    "Я остановился",
+    "для закупки",
+    "что и было поручено Савельичу.",
+    "приехал,",
+    "где должен был",
+    "нужных вещей",
+    "отправился по лавкам",
+    "пробыть сутки",
+    "Савельич",
+  ];
+  const totaltext = array.join(" ");
+  console.log(totaltext);
 }
 
-task1()
+task3();
+
+function task4(firstName, lastName) {
+  const fullName = `${firstName} ${lastName}`;
+  console.log(fullName);
+}
+
+task4("Andrii", "Belinskii");
+
+function task5() {
+  let a = 21;
+  let b = 67;
+
+  while (a <= b) {
+    a++;
+    if (a % 2 !== 0) {
+      console.log(a);
+    }
+  }
+}
+
+task5();
